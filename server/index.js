ff321d2a1f817b5532415bd29ce29243e41ee848
HakobBasmajyan <105809295+HakobBasmajyan@users.noreply.github.com>
2023-05-31 18:14:04 +0400

    ADD HAKOB

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: ["http://localhost:3001", "https://bbc8-46-70-57-63.ngrok-free.app"],
+    origin: ["http://localhost:3000", `${process.env.REACT_APP_API}`],
     methods: ["PUT", "POST", "GET", "DELETE"],
     credentials: true
 }));
